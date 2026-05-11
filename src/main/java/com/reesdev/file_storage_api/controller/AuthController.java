1a1c84b8027466c4a11a3b682907e57bba543c8d
Riski <reesky.it@gmail.com>
2026-05-11 17:42:06 +0700

    add AuthController for Login User End Point

---
+++ src/main/java/com/reesdev/file_storage_api/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.reesdev.file_storage_api.controller;
+
+import com.reesdev.file_storage_api.dto.LoginRequest;
+import com.reesdev.file_storage_api.dto.LoginResponse;
+import com.reesdev.file_storage_api.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public LoginResponse login(@Valid @RequestBody LoginRequest request){
+        return authService.login(request);
+    }
+}
+
